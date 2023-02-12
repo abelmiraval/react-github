438d23538fc402c3b924d83565970568753083d7
abel miraval <abelmiraval@gmail.com>
2023-02-12 10:15:39 -0500

    Add component user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './user'
+
+function Header({children}){
+  return (
+    <header>
+      <h3>Esto es un header bonito</h3>
+      {children}
+    </header>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+
+      <Header>
+        <ul>
+          <li>
+            <a href='#'>Link 1</a>
+          </li>
+        </ul>
+      </Header>
+
+      <User></User>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hola mundo</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/user.js
@@ -0,0 +1,10 @@
+function User(){
+  return (
+    <div className="User">
+      <img src="avatar" alt=""></img>
+      <p>Nombre del usuario</p>
+    </div>
+  )
+}
+
+export default User
