63a7ca99cf95458ec02d5035b2a7d5bf0868147c
abel miraval <abelmiraval@gmail.com>
2023-02-21 07:47:24 -0500

    5 ways to add CSS

--- src/user.js
+++ src/user.js
@@ -1,23 +1,42 @@
-import { useState } from 'react'
+import { useState } from "react";
+import  style from './user.module.css'
+import './user.scss'
+import styled from 'styled-components'
+
+ const Avatar = styled.img`
+  border: 10px solid black;
+ `
+
+console.log();
 
 
+function User({color, name, avatar, counter }) {
+  const [calculatedName, setCalculatedName] = useState(name);
 
-function User({name, avatar, counter}){
-  const [calculatedName, setCalculatedName] = useState(name)
+  const newName = `${calculatedName} es muy lindo!`;
 
-  const newName = `${calculatedName} es muy lindo!`
-
-  function onClick(){
-    setCalculatedName('New name')
+  function onClick() {
+    setCalculatedName("New name");
   }
 
   return (
-    <div className="User" onClick={onClick}>
-      <img src={avatar} title={name} alt={name}></img>
+    <div
+      style={{
+        borderBottom: `10px solid ${color}`,
+        borderLeft: "50px solid blue",
+      }}
+      className="User"
+      onClick={onClick}
+    >
+      <img className={style.avatar} src={avatar} title={name} alt={name}></img>
+      <img className='avatar' src={avatar} title={name} alt={name}></img>
+      <Avatar src={avatar} title={name} alt={name}></Avatar>
       <p>{newName}</p>
-      <p>Hemos dado click en {newName} por {counter}</p>
+      <p>
+        Hemos dado click en {newName} por {counter}
+      </p>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
