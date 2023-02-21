cca1c66baa757b94fdfa40abd45e561641d8624e
abel miraval <abelmiraval@gmail.com>
2023-02-21 14:19:58 -0500

    Add project section

--- src/App.js
+++ src/App.js
@@ -1,64 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import User from './user'
-import { useState } from 'react'
-
-function Header({children}){
-  return (
-    <header>
-      <h3>Esto es un header bonito</h3>
-      {children}
-    </header>
-  )
-}
+import Filters from './components/filters';
+import Layout from './components/layout';
+import Profile from './components/profile';
+import RepoList from './components/repo-list';
+import Search from './components/search';
 
 function App() {
 
-  const  [counter, setCounter] = useState(0)
-
-  function handleSubmit(event){
-    event.preventDefault()
-    console.log('submit')
-  }
-
-
   return (
-    <div className="App" onClick={()=> {
-      setCounter(counter + 1)
-    }}>
-
-      <Header>
-        <ul>
-          <li>
-            <a href='#'>Link 1</a>
-          </li>
-        </ul>
-      </Header>
-
-      <User color="purple" counter={counter} name="La naturaleza" avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ3TnikLSSqhYYwqm2a9J5QHpCtg3Uhejp_Q&usqp=CAU"></User>
-
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text"/>
-        <input type="text"/>
-        <input type="submit"/>
-      </form>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola mundo</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+        <Profile></Profile>
+        <Filters></Filters>
+        <RepoList></RepoList>
+        <Search></Search>
+    </Layout>
   );
 }
 
