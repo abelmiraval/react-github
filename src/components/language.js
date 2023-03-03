5bb94e17a8ceb8e61da7075575c3737ea5ed5ab9
abel miraval <abelmiraval@gmail.com>
2023-03-02 19:45:10 -0500

    Add component language main

---
+++ src/components/language.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+const LanguageStyled = styled.div`
+  display: flex;
+  gap: .5rem;
+  align-items: center;
+  &:before{
+    content: '';
+    inline-size: 1rem;
+    block-size: 1rem;
+    border-radius: 50%;
+    background: ${({ color }) => color}
+  }
+`
+
+const languages = {
+  ruby: {
+    color: 'red',
+  },
+  css: {
+    color: 'green',
+  },
+  javascript: {
+    color: 'yellow',
+  }
+}
+
+function Language({name}) {
+  const formatedName = name.toLowerCase()
+  const color = languages[formatedName] ?  languages[formatedName].color : 'white';
+  return (
+    <LanguageStyled color={color}>
+      {name}
+    </LanguageStyled>
+  )
+}
+
+export default Language
--- src/components/repo-item.js
+++ src/components/repo-item.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Language from './language'
 
 const RepoItemStyled = styled.div`
   padding-block: 1rem;
@@ -79,6 +80,9 @@ function RepoItem(props) {
             }
           </div>
         ) : null
+      }
+      {
+        props.language ? <Language name={props.language} /> : null
       }
     </RepoItemStyled>
   )
