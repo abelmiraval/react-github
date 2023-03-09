55b8d257b231f05a72d9cefbd4d4230e062783a5
abel miraval <abelmiraval@gmail.com>
2023-03-09 07:32:26 -0500

    Add contrast button and Input Text

--- src/modal.js
+++ src/modal.js
@@ -1,29 +1,52 @@
-import {useRef} from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Overlay from "./overlay";
+import {ButtonContrast} from "./components/button";
+import InputText from "./components/input-text";
 
+const ModalContentStyled = styled.form`
+  background: var(--bg);
+  color: var(--white);
+  padding: 1.5rem;
+  border-radius: .5rem;
+  position: fixed;
+  inset-block-start: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  inset-inline-start: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  inline-size: 24rem;
+  .title{
+    font: var(--headline2-semi-bold);
+    margin: 0;
+  }
+`
 
-function ModalContent(){
-
+function ModalContent() {
   const form = useRef(null);
-  const navigator = useNavigate()
-  console.log({form });
+  const navigator = useNavigate();
+  console.log({ form });
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log({form})
+    console.log({ form });
 
-    const formData = new FormData(form.current)
-    console.log(formData.get('username'))
-    navigator(`/${formData.get('username')}`)
+    const formData = new FormData(form.current);
+    console.log(formData.get("username"));
+    navigator(`/${formData.get("username")}`);
   }
 
   return (
-    <form ref={form} action="" onSubmit={handleSubmit}>
-      <h2>Busca a tu usuario</h2>
-      <input type="text" name="username" placeholder="Username" />
-      <button>Buscar</button>
-    </form>
-  )
+    <Overlay>
+      <ModalContentStyled ref={form} action="" onSubmit={handleSubmit}>
+        <h2 className="title">Busca a tu usuario favorito</h2>
+        <InputText type="text" autoComplete='off' name="username" placeholder="Username" />
+        <ButtonContrast text="Buscar"/>
+      </ModalContentStyled>
+    </Overlay>
+  );
 }
 
-export default ModalContent
+export default ModalContent;
